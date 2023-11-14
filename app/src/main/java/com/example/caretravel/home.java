99986d6b3c6faa4f045b1595d828c22cf0537afe
park-hyun-ji <phyunj717@naver.com>
2023-11-14 23:56:28 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/caretravel/home.java
+++ app/src/main/java/com/example/caretravel/home.java
@@ -3,12 +3,19 @@ package com.example.caretravel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.caretravel.databinding.ActivityHomeBinding;
+import com.example.caretravel.databinding.ActivityMainBinding;
 
 public class home extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
+    Button materialButton = findViewById(R.id.material_Button);
+
 }
