f0eac161dd720556496bba35d5d464269b093be7
park-hyun-ji <phyunj717@naver.com>
2023-11-16 13:28:15 +0900

    hyun manifest minsdk 25로 내렸숨니당

--- app/src/main/java/com/example/caretravel/home.java
+++ app/src/main/java/com/example/caretravel/home.java
@@ -1,7 +1,9 @@
 package com.example.caretravel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +17,60 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        binding.backButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                finish();
+//            }
+//        });
+
+        binding.myInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(home.this,myPage.class));
+            }
+        });
+
+        binding.materialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(home.this,material.class));
+            }
+        });
+
+        binding.planButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(home.this,make_Plan.class));
+            }
+        });
+
+        binding.calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(home.this,calculation.class));
+            }
+        });
+
+        binding.pathButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(home.this,path.class));
+            }
+        });
+
+        binding.travelInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(home.this,activity_travel_info.class));
+            }
+        });
+
     }
-    Button materialButton = findViewById(R.id.material_Button);
 
 }
