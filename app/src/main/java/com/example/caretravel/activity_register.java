9bbf3d5385064e9e5aaf922456e93ab21be92dac
park-hyun-ji <phyunj717@naver.com>
2023-11-20 14:26:02 +0900

    naya

--- app/src/main/java/com/example/caretravel/activity_register.java
+++ app/src/main/java/com/example/caretravel/activity_register.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -50,7 +52,33 @@ public class activity_register extends AppCompatActivity {
 //        }
 //    };
 
-    @Override
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK) {
+                        createBtn();
+                    }
+                }
+            });
+
+    public  void createBtn () {
+        String roomName = getIntent().getStringExtra("documentName");
+
+        RelativeLayout View = (RelativeLayout) findViewById(R.id.relativeView);
+        Log.d("phj","뷰 정해줌");
+        Button roomButton = new Button(activity_register.this);
+        Log.d("phj","버튼생성");
+        roomButton.findViewById(R.id.roomRegister_button);
+        Log.d("phj","findview");
+        roomButton.setText(roomName);
+        Log.d("phj","setText");
+        View.addView(roomButton);
+        Log.d("phj","addView");
+    }
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
@@ -71,7 +99,6 @@ public class activity_register extends AppCompatActivity {
         });
 
 
-
 //        binding.roomButton.setOnClickListener(view -> {
 //            //custom dialog를 위한 layout xml 초기화
 //            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
@@ -87,32 +114,11 @@ public class activity_register extends AppCompatActivity {
 //        });
     }
 
-    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK) {
-                        String roomName = getIntent().getStringExtra("documentName");
-                        Log.d("phj","버튼 만들면됨");
-
-//                        LayoutInflater inflater = LayoutInflater.from(activity_register.this);
-//                        View view = inflater.inflate(R.layout.room_button, null);
-//                        View addbuttonview = findViewById(R.id.addButtonView);
-//                        View.addView(addbuttonview);
-
-
-//                        View view = (View) findViewById(R.id.addButtonView);
-//                        Button roomButton = new Button(this);
-//                        roomButton.setText(roomName);
-//                        view.addView(roomButton);
-
-                        LinearLayout View = (LinearLayout) findViewById(R.id.roomRegister_button);
-                        Button roomButton = new Button(activity_register.this);
-                        roomButton.setText(roomName);
-                        View.addView(roomButton);
-                    }
-                }
-            });
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //for (서버에 있는 방 다 만들때까지)
+        createBtn();
+        //방 이름 만들때 넘버링 할까????????
+    }
 }
