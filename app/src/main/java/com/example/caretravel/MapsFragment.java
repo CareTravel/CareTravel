e62da732851f84d0e9931c61ab8868725b43433b
hyunji <parkhyunji5486@gmail.com>
2023-12-12 19:46:59 +0900

    지도

--- app/src/main/java/com/example/caretravel/MapsFragment.java
+++ app/src/main/java/com/example/caretravel/MapsFragment.java
@@ -3,12 +3,16 @@ package com.example.caretravel;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentResultListener;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,20 +26,16 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsFragment extends Fragment {
-    private FirebaseFirestore db;
+    //private FirebaseFirestore db;
+
+    private GoogleMap mMap;
+    private Geocoder geocoder;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         @Override
         public void onMapReady(GoogleMap googleMap) {
 //            int n = getArguments().size(); //리스트
@@ -44,9 +44,48 @@ public class MapsFragment extends Fragment {
 //                double lat = ;//저장된 변수 이름
 //                double lng = ;//저장된 변수 이름
 
-                LatLng day = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions().position(day).title("Marker in Sydney"));
-                googleMap.moveCamera(CameraUpdateFactory.newLatLng(day));
+            mMap = googleMap;
+            geocoder = new Geocoder(getActivity());
+
+            String str = "숭실대학교 정보과학관";
+            //String str=editText.getText().toString();
+            List<Address> addressList = null;
+            try {
+                // editText에 입력한 텍스트(주소, 지역, 장소 등)을 지오 코딩을 이용해 변환
+                addressList = geocoder.getFromLocationName(
+                        str, // 주소
+                        10); // 최대 검색 결과 개수
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(addressList.get(0).toString());
+            // 콤마를 기준으로 split
+            String []splitStr = addressList.get(0).toString().split(",");
+            String address = splitStr[0].substring(splitStr[0].indexOf("\"") + 1,splitStr[0].length() - 2); // 주소
+            System.out.println(address);
+
+            String latitude = splitStr[10].substring(splitStr[10].indexOf("=") + 1); // 위도
+            String longitude = splitStr[12].substring(splitStr[12].indexOf("=") + 1); // 경도
+            System.out.println(latitude);
+            System.out.println(longitude);
+
+            // 좌표(위도, 경도) 생성
+            LatLng point = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+            // 마커 생성
+            MarkerOptions mOptions2 = new MarkerOptions();
+            mOptions2.title(str);
+            mOptions2.snippet(address);
+            mOptions2.position(point);
+            // 마커 추가
+            mMap.addMarker(mOptions2);
+            // 해당 좌표로 화면 줌
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point,20));
+
+//                LatLng day = new LatLng(-34, 151);
+//                googleMap.addMarker(new MarkerOptions().position(day).title("Marker in Sydney"));
+//                googleMap.moveCamera(CameraUpdateFactory.newLatLng(day));
             }
 //        }
     };
@@ -94,9 +133,18 @@ public class MapsFragment extends Fragment {
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+            mapFragment.getMapAsync(callback);}
 
 
-        }
+        Button button = getView().findViewById(R.id.fragment_off);
+        button.setOnClickListener(buttonListener);
+
     }
+    View.OnClickListener buttonListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            FragmentManager manager = getActivity().getSupportFragmentManager();
+            manager.beginTransaction().remove(MapsFragment.this).commit();
+            manager.popBackStack();
+        }};
 }
