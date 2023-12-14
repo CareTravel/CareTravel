46f722097749d81aa1f11d4090bb83806c2c1861
hyunji <parkhyunji5486@gmail.com>
2023-12-14 14:11:54 +0900

    지도

--- app/src/main/java/com/example/caretravel/MapsFragment.java
+++ app/src/main/java/com/example/caretravel/MapsFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentResultListener;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,32 +25,81 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class MapsFragment extends Fragment {
-    //private FirebaseFirestore db;
-
+    private FirebaseFirestore db;
     private GoogleMap mMap;
     private Geocoder geocoder;
+
+    String roomName;
+    String documentName;
+    List<Map<String, Object>> locateList;
+
+
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
         @Override
         public void onMapReady(GoogleMap googleMap) {
-//            int n = getArguments().size(); //리스트
-//            //여러개 동시에 마커 찍을 수 있게 리스트로 가져와서 i는 장소 개수(행 수)
-//            for(int i=0; i<n; i++) {
-//                double lat = ;//저장된 변수 이름
-//                double lng = ;//저장된 변수 이름
-
             mMap = googleMap;
             geocoder = new Geocoder(getActivity());
 
-            String str = "숭실대학교 정보과학관";
-            //String str=editText.getText().toString();
-            List<Address> addressList = null;
+            Log.d("phj",roomName);
+            Log.d("phj",documentName);
+
+            db.collection("rooms").document(roomName).collection("경로").document(documentName)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Log.d("phj", "if task");
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d("phj", "document exists");
+
+                                // "List" 필드의 존재 여부 확인
+                                if (document.contains("List")) {
+                                    Log.d("phj", "contain list");
+
+                                    // price 필드에 저장된 리스트 가져오기
+                                    locateList = (List<Map<String, Object>>) document.get("List");
+
+                                    // price 값만 추출하여 더하기
+                                    for (Map<String, Object> list : locateList) {
+                                        String locate = (String) list.get("locate");
+                                        if (locate.equals("")) {
+                                        } else {
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (!task.isSuccessful()) {
+                            Exception e = task.getException();
+                            Log.e("phj", "Error getting document", e);
+                        }
+                    });
+
+            if(locateList != null){
+                Log.d("phj", " 리스트 null");
+            }
+
+
+            int n = locateList.size();
+            Log.d("phj", String.valueOf(n));
+
+//            //여러개 동시에 마커 찍을 수 있게 리스트로 가져와서 i는 장소 개수(행 수)
+            for(int i=0; i<n; i++) {
+
+            //리스트에서 장소 포문 돌면서 가져오기
+            String str = locateList.get(i).toString();
+                Log.d("phj",str);
+
+                List<Address> addressList = null;
             try {
                 // editText에 입력한 텍스트(주소, 지역, 장소 등)을 지오 코딩을 이용해 변환
                 addressList = geocoder.getFromLocationName(
@@ -81,13 +131,11 @@ public class MapsFragment extends Fragment {
             // 마커 추가
             mMap.addMarker(mOptions2);
             // 해당 좌표로 화면 줌
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point,20));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point,15));
 
-//                LatLng day = new LatLng(-34, 151);
-//                googleMap.addMarker(new MarkerOptions().position(day).title("Marker in Sydney"));
-//                googleMap.moveCamera(CameraUpdateFactory.newLatLng(day));
             }
-//        }
+        //아래 포문 괄호
+        }
     };
 
     @Nullable
@@ -95,40 +143,16 @@ public class MapsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        roomName = getArguments().getString("roomname");
+        documentName = getArguments().getString("day");
         return inflater.inflate(R.layout.fragment_maps_path, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        db = FirebaseFirestore.getInstance();
 
-//        //n일차에 맞는 장소 정보를 파바에서 가져오기
-//        db = FirebaseFirestore.getInstance();
-//        db.collection("rooms").document(roomId).collection(path)
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@io.reactivex.rxjava3.annotations.NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (DocumentSnapshot document : task.getResult()) {
-//                                String roomName = document.getId();
-//                            }
-//                        }
-//                    }
-//                });
-//        //여기까지
-
-//        //저장해둔 값 가져오기
-//        getParentFragmentManager().setFragmentResultListener("requestKey", this,
-//                new FragmentResultListener() {
-//                    @Override
-//                    public void onFragmentResult(@NonNull String key, @NonNull Bundle bundle) {
-//                        // 번들 키 값 입력
-//                        String day = bundle.getString("bundleKey");
-//                        // 전달 받은 result 이용하여 코딩
-//                    }
-//                });
-//        // 여기까지
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
@@ -147,4 +171,33 @@ public class MapsFragment extends Fragment {
             manager.beginTransaction().remove(MapsFragment.this).commit();
             manager.popBackStack();
         }};
+
+//    public void loadLocate(){
+//    db = FirebaseFirestore.getInstance();
+//            db.collection("rooms").document(roomName).collection("경로").document(day)
+//                    .get()
+//                    .addOnCompleteListener(task -> {
+//            if (task.isSuccessful()) {
+//                DocumentSnapshot document = task.getResult();
+//                if (document.exists()) {
+//                    // "List" 필드의 존재 여부 확인
+//                    if (document.contains("List")) {
+//                        // price 필드에 저장된 리스트 가져오기
+//                        locateList = (List<Map<String, Object>>) document.get("List");
+//
+//                        // price 값만 추출하여 더하기
+//                        for (Map<String, Object> list : locateList) {
+//                            int a;
+//                            String price = (String) list.get("locate");
+//                            if (price.equals("")) {
+//                                a = 0;
+//                            } else {
+//                                a = Integer.parseInt(price);
+//                            }
+//                        }
+//                    }
+//                }
+//        }
+//                    });
+//    }
 }
